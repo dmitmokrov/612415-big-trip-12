a3c820a3f8011ffe5a6c2b0a801911b4ef1b9521
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-08-17 19:05:34 +0300

    Рефакторинг отображения блока с информацией при отсутствии задач

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,19 +1,9 @@
 import {getFormatDate, createElement} from '../utils.js';
 
 const createTripInfoElement = (trips) => {
-  if (trips.length === 0) {
-    return `<section class="trip-main__trip-info  trip-info">
-    <div class="trip-info__main">
-      <h1 class="trip-info__title"></h1>
-
-      <p class="trip-info__dates"></p>
-    </div>
-  </section>`;
-  }
-
   const firstLocation = trips[0];
   const lastLocation = trips[trips.length - 1];
-  const dates = `${getFormatDate(firstLocation.startTime)}&nbsp;&mdash;&nbsp;${getFormatDate(lastLocation.startTime)}`;
+  const dates = trips.length ? `${getFormatDate(firstLocation.startTime)}&nbsp;&mdash;&nbsp;${getFormatDate(lastLocation.startTime)}` : ``;
   const uniqueLocations = [...new Set(trips.map((trip) => trip.destination))];
   const title = (uniqueLocations.length > 3) ? `${firstLocation.destination} &mdash;...&mdash; ${lastLocation.destination}` : uniqueLocations.join(`-`);
 
